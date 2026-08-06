1b13923ef4cdaabd815c40c875cd1d1964d2ef41
aayushipandya333 <aayushipandya333@gmail.com>
2026-08-06 17:30:20 +0530

    lead module

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
+# This file works around : Creating Application → Connect Database → Allow CORS → Register APIs → Ready to start Server 
+
+
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware              # A middleware runs before your request reaches your API and before the response goes back to the client. CORS - Cross-Origin Resource Sharing. a browser security feature that controls whether a frontend running on one origin (domain/port/protocol) can access a backend running on another origin.
 from app.core.config import settings
 from app.core.database import engine, Base
 from app.api import tasks
@@ -7,7 +10,7 @@ from app.api import leads
 from app.models.task import Task
 from app.models.lead import Lead
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)                       # instructs SQLAlchemy to create all tables defined by models that inherit from Base. It uses the engine to connect to the configured database and creates only the tables that do not already exist.
 
 app = FastAPI(
     title = "Agency OS API",
@@ -17,11 +20,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_origins=[settings.FRONTEND_URL],                
+    allow_credentials=True,                                # Allow Authorization, Cookies, JWT
+    allow_methods=["*"],                                   # allow all methods : GET, POST, PUT, PATCH, DELETE
     allow_headers=["*"],
 )
 
-app.include_router(tasks.router)
-app.include_router(leads.router)
+app.include_router(tasks.router)                            # register all endpoints of api -> tasks.py into application
+app.include_router(leads.router)                            # register all endpoints of api -> leads.py into application
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,23 @@
 //File which acts as a bridge between frontend and backend
+//Without this file: Every React component would have to write fetch() code.
+//But now, by using thisfile, every component simply calls functions like: getTasks(), createTask(), deleteTask()
+//This follows the DRY (Don't Repeat Yourself) principle.
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000"                         // To store base url of backend. Access the backend url either with .env file (If mentioned there) or use direct localhost link provided after ||
 
-export async function fetchWithAuth(endpoint, getToken, options = {}) {
+export async function fetchWithAuth(endpoint, getToken, options = {}) {                        // This function Fetch data with authentication. This function automatically: Gets the Clerk token, Adds it to the request, Calls the backend, Handles errors, Returns data.
+                                                                                               // endpoint : It tells the function which backend API to call. "/api/tasks" or "/api/leads" 
+                                                                                               // getToken : This comes from Clerk.
+                                                                                               // options = {} : Options is for third argument. If third argument is passed (like in post request), then its good else if no third argument is passed (Like in get request), JavaScript automatically uses {} i.e. an empty object.
     const token = await getToken()
-
-    const response = await fetch(
-        `${API_URL}${endpoint}`,
-         {
-            ...options,
-            headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-            ...options.headers
+    const response = await fetch(                                                              // fetch() is a built-in JavaScript function used to send HTTP requests to a server (your FastAPI backend).
+        `${API_URL}${endpoint}`,                                                                // First argument which is passed to fetch(). It creates the complete URL that fetch() will call. To build url dynamically.
+         {                                                                                      // fetch() accepts two main arguments: fetch(URL, options). URL (where to send the request), Options (how to send the request). 
+            ...options,                                                                         // ...options : It copied every property from options, we received in above function.
+            headers: {                                                                          // Headers are metadata sent with an HTTP request
+            'Content-Type': 'application/json',                                                 // Specifies that JSON data is sent to backend
+            'Authorization': `Bearer ${token}`,                                                 // Tell the backend who the user is.
+            ...options.headers                                                                  // If any additional headers to be provided in future, then they will also be added.
             }
         }
     )
@@ -22,11 +27,11 @@ export async function fetchWithAuth(endpoint, getToken, options = {}) {
         throw new Error(error.detail || "Request failed")
     }
 
-    if (response.status == 204){
+    if (response.status == 204){                                                                   // 204 ---> no content. Mostly used in delete. If deleted, then return nothing (i.e null)
         return null
     }
 
-    return response.json()
+    return response.json()                                                                           // if no error, then return the response came from backend
 }
 
 
@@ -34,14 +39,14 @@ export async function fetchWithAuth(endpoint, getToken, options = {}) {
 
 // ---------- TASKS ----------
 
-export async function getTasks(getToken) {
+export async function getTasks(getToken) {                                                            // exported from here so that other pages can import it. like in KanbanBoard.jsx ---> import {createTask, updateTask, deleteTask} from "../services/api.js"
     return fetchWithAuth("/api/tasks", getToken)
 } 
 
 export async function createTask(getToken, task) {
     return fetchWithAuth("/api/tasks", getToken, {
         method: "POST",
-        body : JSON.stringify(task)
+        body : JSON.stringify(task)                                                                   // task is an object and browser can't send object thus convert it into JSON string 
     })
 } 
 
